5b49902c7040edc03b36cade12c4e45b9b90a015
Ryan Arora <arora.ryan@gmail.com>
2021-10-23 14:02:02 -0400

    fix: remove unnessecary console.log

--- server/src/middleware/isAuthenticated.ts
+++ server/src/middleware/isAuthenticated.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-  console.log("isAuthenticated");
-
   if (!req.session.userId) {
     res.status(401).json({
       message: "Unauthorized.",
