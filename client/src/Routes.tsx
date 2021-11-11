a7063834a4bf5ac0b4807db09095610e72721078
1bandisre <73853943+1bandisre@users.noreply.github.com>
2021-11-10 22:17:31 -0500

    Update Routes.tsx

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+//Importing all of the components and pages to make them functional and connect to the backend 
 import Header from "./ui/components/Header";
 import Login from "./ui/pages/Login";
 import NotFound from "./ui/pages/NotFound";
@@ -11,10 +11,12 @@ import axios from "axios";
 import { useGlobalState, Token } from "./index";
 import Landing from "./ui/pages/Landing";
 
+//This function is checking for authentication with credentials for the dashboard etc, and then allowing the user to access pages
 const Routes: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useGlobalState("isLoggedIn");
   const [tokens, setTokens] = useGlobalState("tokens");
 
+  //Catching errors and checking for authentication for page redirection  
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_SERVER_URL}/getTokens`, { withCredentials: true })
@@ -22,18 +24,17 @@ const Routes: React.FC = () => {
         const data: any = res.data;
         const newTokens: Token[] = data.tokens;
 
-        console.log(newTokens);
         setTokens(newTokens);
         setIsLoggedIn(true);
       })
       .catch((err) => {
         if (err?.response?.status === 401) {
-          console.log("401 :)");
           setIsLoggedIn(false);
         }
       });
   }, []);
-
+//Setting up the routes so that the user can view different pages.
+//Allows the user to move between different parts of an application when they enters a URL or clicks an element 
   return (
     <Router>
       <Header />
